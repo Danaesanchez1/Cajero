3e2139de192154e00faed1b6bd09ea485d83992e
Danae <danaesanchez142003@gmail.com>
2023-11-09 00:57:15 -0300

    tengo listo mi formulario

--- scripts.js
+++ scripts.js
@@ -1,10 +1,80 @@
-var cuentas = [
-  { nombre: "Persona 1", saldo: 200 },
-  { nombre: "Persona 2", saldo: 290 },
-  { nombre: "Persona 3", saldo: 67 }
-];
+document.addEventListener("DOMContentLoaded", function () {
+    const cuentas = [
+        { nombre: "Felipe", saldo: 200, password: "1234" },
+        { nombre: "Camila", saldo: 290, password: "1234" },
+        { nombre: "Danae", saldo: 67, password: "1234" }
+    ];
 
-var cuentaSeleccionada = null;
+    let cuentaSeleccionada = null;
 
+    // Obtener elementos del DOM
+    const selectCuenta = document.querySelector('.cuenta');
+    const inputPassword = document.getElementById('password');
+    const botonIngresar = document.getElementById('botonIngresar');
+    const operaciones = document.getElementById('operaciones');
+    const consultarBtn = document.getElementById('consultar');
+    const ingresarBtn = document.getElementById('ingresar');
+    const retirarBtn = document.getElementById('retirar');
+    const resultado = document.getElementById('resultado');
 
+    // Llenar select con las opciones de cuentas
+    cuentas.forEach((cuenta, index) => {
+        const option = document.createElement('option');
+        option.value = index;
+        option.textContent = cuenta.nombre;
+        selectCuenta.appendChild(option);
+    });
+
+    botonIngresar.addEventListener('click', () => {
+        const opcion = selectCuenta.value;
+        const password = inputPassword.value;
+
+        if (cuentas[opcion].password === password) {
+            cuentaSeleccionada = cuentas[opcion];
+            operaciones.style.display = "block";
+            resultado.textContent = `Hola, ${cuentaSeleccionada.nombre}. Selecciona una operación.`;
+        } else {
+            resultado.textContent = "Contraseña incorrecta. Inténtelo de nuevo.";
+        }
+    });
+
+    consultarBtn.addEventListener('click', () => {
+        if (cuentaSeleccionada) {
+            resultado.textContent = `Su saldo actual es: $${cuentaSeleccionada.saldo}`;
+        }
+    });
+
+    ingresarBtn.addEventListener('click', () => {
+        if (cuentaSeleccionada) {
+            let monto = prompt("Ingrese el monto que desea abonar a su cuenta:");
+            monto = parseInt(monto);
+
+            if (monto > 0) {
+                cuentaSeleccionada.saldo += monto;
+                if (cuentaSeleccionada.saldo > 990) {
+                    alert("No puede tener más de $990 en su cuenta, intente nuevamente.")
+                    cuentaSeleccionada.saldo -= monto;
+                } else {
+                    resultado.textContent = `Se han ingresado $${monto}. Su saldo actual es: $${cuentaSeleccionada.saldo}`;
+                }
+            } else {
+                resultado.textContent = "Ingrese un monto válido.";
+            }
+        }
+    });
+
+    retirarBtn.addEventListener('click', () => {
+        if (cuentaSeleccionada) {
+            let monto = prompt("Ingrese el monto a retirar:");
+            monto = parseInt(monto);
+
+            if (monto > 0 && cuentaSeleccionada.saldo - monto >= 10) {
+                cuentaSeleccionada.saldo -= monto;
+                resultado.textContent = `Se han retirado $${monto}. Su saldo actual es: $${cuentaSeleccionada.saldo}`;
+            } else {
+                alert("Monto inválido o excede su límite de retiro.");
+            }
+        }
+    });
+});
 
